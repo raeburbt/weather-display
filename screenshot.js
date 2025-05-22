056adc108ddd38e90818b477b3fcf370823c1664
raeburbt <107575524+raeburbt@users.noreply.github.com>
2025-05-22 09:37:54 -0600

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -7,9 +7,9 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
 // …after newPage()
-await page.setViewport({ width: 1805, height: 1015 });
+await page.setViewport({ width: 2005, height: 1128 });
 await page.goto('https://raeburbt.github.io/weather-display/', { waitUntil: 'networkidle2' });
-const clip = { x: 0, y: 0, width: 1805, height: 1015 };
+const clip = { x: 0, y: 0, width: 2005, height: 1128 };
 await page.screenshot({ path: 'weather.png', clip });
 
 
