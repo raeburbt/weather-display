21e6c75fbbcb3cfab84cedc9af1d4e9978ac4b6e
raeburbt <107575524+raeburbt@users.noreply.github.com>
2025-06-24 15:20:06 -0600

    Update screenshot-mammoth.js

--- screenshot-mammoth.js
+++ screenshot-mammoth.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
 // …after const page = await browser.newPage();
-await page.setViewport({ width: 1299, height: 731 });
+await page.setViewport({ width: 1920, height: 1080 });
 await page.goto(
   'https://raeburbt.github.io/weather-display/index-mammoth.html',
   { waitUntil: 'networkidle2' }
