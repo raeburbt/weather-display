bd8145730110401975fbc198b2a22fda1b61db21
yelldigital <yell_digital@nps.gov>
2026-02-13 09:06:50 -0700

    Update screenshot-mammoth.js

--- screenshot-mammoth.js
+++ screenshot-mammoth.js
@@ -8,7 +8,7 @@ const puppeteer = require('puppeteer');
 // …after const page = await browser.newPage();
 await page.setViewport({ width: 1920, height: 1080 });
 await page.goto(
-  'https://raeburbt.github.io/weather-display/index-mammoth.html',
+  'https://yelldigital.github.io/weather-display/index-mammoth.html',
   { waitUntil: 'networkidle2' }
 );
 await page.waitForSelector('.period', { timeout: 10000 });
